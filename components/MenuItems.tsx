763e580b78392c1310f94545ac82f27272465524
lennne <kobiansah@gmail.com>
2025-08-07 12:17:32 -0230

    minor update: rendered a large list with more data

--- components/MenuItems.tsx
+++ components/MenuItems.tsx
@@ -1,48 +1,62 @@
 import { SectionList, StyleSheet, Text, View } from "react-native";
 
 const menuItemsToDisplay = [
-    {
-        title: 'Appetizers',
-        data: [
-            'Hummus',
-            'Moutabal',
-            'Falafel',
-            'Marinated Olives',
-            'Kofta',
-            'Eggplant Salad'
-        ],
-    },{
-        title: 'Main Dishes',
-        data: ['Lentil Burger', 'Smoked Salmon', 'Kofta Burger', 'Turkish Kebab'],
-    },
-    {
-        title: 'Sides',
-        data: [
-            'Fries',
-            'Buttered Rice',
-            'Bread Sticks',
-            'Pita Pocket',
-            'Lentil Soup',
-            'Greek Salad',
-        ],
-    },
-    {
-        title: 'Desserts',
-        data: ['Baklava', 'Tartufo', 'Tiramisu', 'Panna Cotta'],
-    }
-]
+  {
+    title: 'Appetizers',
+    data: [
+      { name: 'Hummus', price: '$5.00' },
+      { name: 'Moutabal', price: '$5.00' },
+      { name: 'Falafel', price: '$7.50' },
+      { name: 'Marinated Olives', price: '$5.00' },
+      { name: 'Kofta', price: '$5.00' },
+      { name: 'Eggplant Salad', price: '$8.50' },
+    ],
+  },
+  {
+    title: 'Main Dishes',
+    data: [
+      { name: 'Lentil Burger', price: '$10.00' },
+      { name: 'Smoked Salmon', price: '$14.00' },
+      { name: 'Kofta Burger', price: '$11.00' },
+      { name: 'Turkish Kebab', price: '$15.50' },
+    ],
+  },
+  {
+    title: 'Sides',
+    data: [
+      { name: 'Fries', price: '$3.00', id: '11K' },
+      { name: 'Buttered Rice', price: '$3.00' },
+      { name: 'Bread Sticks', price: '$3.00' },
+      { name: 'Pita Pocket', price: '$3.00' },
+      { name: 'Lentil Soup', price: '$3.75' },
+      { name: 'Greek Salad', price: '$6.00' },
+      { name: 'Rice Pilaf', price: '$4.00' },
+    ],
+  },
+  {
+    title: 'Desserts',
+    data: [
+      { name: 'Baklava', price: '$3.00' },
+      { name: 'Tartufo', price: '$3.00' },
+      { name: 'Tiramisu', price: '$5.00' },
+      { name: 'Panna Cotta', price: '$5.00' },
+    ],
+  },
+];
 
 type ItemProps = {
-    name: string
+    name: string;
+    price: string;
 }
 
 type ItemHeader = {
     section: { title: string};
 }
 
-const Item = ( {name} : ItemProps) => (
+const Item = ( {name, price} : ItemProps) => (
     <View style={menuStyles.innerContainer}>
         <Text style={menuStyles.itemText}>{name}</Text>
+        <Text style={menuStyles.itemText}>{price}</Text>
     </View>
 )
 
@@ -51,12 +65,12 @@ const SectionHeader = ({ section: {title} }: ItemHeader) => (
 )
 
 export default function MenuItems (){
-    const renderItem = ({item} : {item:string}) => <Item name={item} />
+    const renderItem = ({item} : {item: ItemProps}) => <Item name={item.name} price={item.price} />
     return (
         <View style={menuStyles.container}>
             <SectionList
                 sections={menuItemsToDisplay}
-                keyExtractor={(item, index) => item + index}
+                keyExtractor={(item, index) => item.name + index}
                 renderItem={renderItem}
                 renderSectionHeader={SectionHeader}
         >
@@ -67,31 +81,24 @@ export default function MenuItems (){
 
 const menuStyles = StyleSheet.create({
   container: {
-    flex: 0.95,
+    flex: 1,
   },
   innerContainer: {
     paddingHorizontal: 40,
     paddingVertical: 20,
-    backgroundColor: '#333333',
-  },
-  sectionHeader: {
-    backgroundColor: '#fbdabb',
-    color: '#333333',
-    fontSize: 34,
-    flexWrap: 'wrap',
-    textAlign: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   },
   itemText: {
     color: '#F4CE14',
-    fontSize: 32,
-  },
-  separator: {
-    borderBottomWidth: 1,
-    borderColor: '#EDEFEE',
-  },
-  footerText: {
-    color: '#EDEFEE',
     fontSize: 20,
+  },
+  headerStyle: {
+    backgroundColor: '#F4CE14',
+  },
+  sectionHeader: {
+    color: 'black',
+    fontSize: 26,
     flexWrap: 'wrap',
     textAlign: 'center',
   },
