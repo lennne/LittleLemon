80c27de2e07b8b48787494976b87d0a6ad82fa0d
lennne <kobiansah@gmail.com>
2025-08-06 23:53:52 -0230

    minor update: render a large list using flatlist

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,7 @@ import LittleLemonFooter from '@/components/LittleLemonFooter';
 import LittleLemonHeader from '@/components/LittleLemonHeader';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import WelcomeScreen from '@/components/WelcomeScreen';
+import MenuItems from '@/components/MenuItems';
 
 
 
@@ -12,7 +13,7 @@ export default function HomeScreen() {
    <SafeAreaView  style={{flex:1}}>    
    <View style={styles.container}>
     <LittleLemonHeader />
-    <WelcomeScreen />
+    <MenuItems />
     </View>
      
     <View style={styles.footer}>
--- components/MenuItems.tsx
+++ components/MenuItems.tsx
@@ -1,38 +1,74 @@
-import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { FlatList, ScrollView, StyleSheet, Text, View } from "react-native";
 
 const menuItemsToDisplay = [
-    'Hummus \n Moutabal \n Falafel \n Marinated Olives \n Kofta \n Eggplant Salad \n Lentil Burger \n Smoked Salmon \n Kofta Burger \n Turkish Kebab \n Fries \n Buttered Rice \n Bread Sticks \n Pita Pocket \n Lentil Soup \n Greek Salad \n Rice Pilaf \n Baklava \n Tartufo \n Tiramisu \n Panna Cotta',
+  { name: 'Hummus', price: '$5.00', id: '1A' },
+  { name: 'Moutabal', price: '$5.00', id: '2B' },
+  { name: 'Falafel', price: '$7.50', id: '3C' },
+  { name: 'Marinated Olives', price: '$5.00', id: '4D' },
+  { name: 'Kofta', price: '$5.00', id: '5E' },
+  { name: 'Eggplant Salad', price: '$8.50', id: '6F' },
+  { name: 'Lentil Burger', price: '$10.00', id: '7G' },
+  { name: 'Smoked Salmon', price: '$14.00', id: '8H' },
+  { name: 'Kofta Burger', price: '$11.00', id: '9I' },
+  { name: 'Turkish Kebab', price: '$15.50', id: '10J' },
+  { name: 'Fries', price: '$3.00', id: '11K' },
+  { name: 'Buttered Rice', price: '$3.00', id: '12L' },
+  { name: 'Bread Sticks', price: '$3.00', id: '13M' },
+  { name: 'Pita Pocket', price: '$3.00', id: '14N' },
+  { name: 'Lentil Soup', price: '$3.75', id: '15O' },
+  { name: 'Greek Salad', price: '$6.00', id: '16Q' },
+  { name: 'Rice Pilaf', price: '$4.00', id: '17R' },
+  { name: 'Baklava', price: '$3.00', id: '18S' },
+  { name: 'Tartufo', price: '$3.00', id: '19T' },
+  { name: 'Tiramisu', price: '$5.00', id: '20U' },
+  { name: 'Panna Cotta', price: '$5.00', id: '21V' },
 ];
 
+type ItemProps = {
+    name: string;
+    price: string;
+}
+
+type ItemData = {
+    name: string;
+    price: string;
+    id: string;
+}
+
+const Item = ({ name, price }: ItemProps) => (
+    <View style={menuStyles.innerContainer}>
+        <Text style={menuStyles.itemText}>{name}</Text>
+        <Text style={menuStyles.itemText}>{price}</Text>
+    </View>
+)
+
 export default function MenuItems (){
-    
+    const renderItem = ({item}: {item: ItemData}) => <Item name={item.name} price={item.price} />
+
     return (
         <View style={{flex: 0.75}}>
-            <ScrollView indicatorStyle="white" style={styles.scroll}>
-                <Text style={styles.textHeader}>
-                    View Menu
-                </Text>
-                <Text style={styles.text}>
-                    {menuItemsToDisplay[0]}
-                </Text>
-            </ScrollView>
+            <FlatList 
+            data={menuItemsToDisplay}
+            keyExtractor={(item) => item.id}
+            renderItem={renderItem}
+            >
+            </FlatList>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    scroll: {
+const menuStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    innerContainer: {
         paddingHorizontal: 40,
-        paddingVertical: 40,
-        backgroundColor: "black",
+        paddingVertical: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
     },
-    textHeader: {
-        color: "white",
-        fontSize: 40, 
-        flexWrap: 'wrap'
-    },
-    text: {
+    itemText: {
         color: "#F4CE14",
-        fontSize: 36
+        fontSize: 20, 
     }
 });
